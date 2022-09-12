1c6f6723f84b13cb058e56e8b03e225d4b54132b
creesler <creesler@gmail.com>
2022-09-12 12:08:07 +0800

    updated footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
