5517a5acfe78494b42448ca83e4f380c9e0b361b
creesler <creesler@gmail.com>
2022-09-12 12:18:34 +0800

    added br

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,6 +10,8 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
+                <br/>
+                <br/>
                 <h2 className='flex-row text-4xl sm:text-7xl font-bold text-white'>I'm a</h2>
                 <h2 className='flex-row text-4xl sm:text-1xl font-bold text-white'>
                     <Typewriter 
