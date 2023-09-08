461e94ee32fc7b4001cba81fb1d72a5c0d04acf2
creesler <60352120+creesler@users.noreply.github.com>
2023-09-08 18:58:22 +0800

    Update Portfolio.jsx
    unused codes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,10 @@
 import React, { useState, createRef } from 'react'
-import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
-import installNode from '../assets/portfolio/installNode.jpg'
-import navbar from '../assets/portfolio/navbar.jpg'
-import reactParallax from '../assets/portfolio/reactParallax.jpg'
-import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
-import reactWeather from '../assets/portfolio/reactWeather.jpg'
+// import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
+// import installNode from '../assets/portfolio/installNode.jpg'
+// import navbar from '../assets/portfolio/navbar.jpg'
+// import reactParallax from '../assets/portfolio/reactParallax.jpg'
+// import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
+// import reactWeather from '../assets/portfolio/reactWeather.jpg'
 
 // video player
 import { DefaultPlayer as Video } from 'react-html5video';
